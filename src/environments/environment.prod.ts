92f06abb4c4e51c8ca9a71bcbdf300b666454578
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2026-04-14 10:36:18 -0400

    update git add storefront

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   instagram_token: 'INSTA_TOKEN',
   ENC_KEY: "WmS81fl8F6v3ghnN7VzbrTKbd85H7n4uFwhcFj89j0M=",
   IV: "hMhzs+WOGF1xZwV+L1COeg==",
-  secureUrl: 'https://backendimplementationbralbatech-production.up.railway.app/sec/v1/',
+  secureUrl: 'https://loving-passion-production-157f.up.railway.app/sec/v1/',
   paypal_token: 'AUqwOlb75yY_oKcU0hud9J8Rjc0_dVJJn5qHSeYZJ-eIBk-iQl5dI7o_isPd5l68aP1WjZkZKdvay1_a',
-  baseUrl: 'https://backendimplementationbralbatech-production.up.railway.app/api/v1/',
+  baseUrl: 'https://loving-passion-production-157f.up.railway.app/api/v1/',
   storeUrl: 'https://store.ralbatech.com',
   defaultStore: 'yunicbrightvision'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  secureUrl: 'https://backendimplementationbralbatech-production.up.railway.app/sec/v1/',
-  baseUrl: 'https://backendimplementationbralbatech-production.up.railway.app/api/v1/',
+  secureUrl: 'https://loving-passion-production-157f.up.railway.app/sec/v1/',
+  baseUrl: 'https://loving-passion-production-157f.up.railway.app/api/v1/',
 
   instagram_token: 'INSTA_TOKEN',
   ENC_KEY: "WmS81fl8F6v3ghnN7VzbrTKbd85H7n4uFwhcFj89j0M=",
